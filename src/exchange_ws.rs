3a3329695b2aee304fcbfe46ab4b034899e1564b
aurel <aurelien.catinon@gmail.com>
2020-09-09 19:05:58 +0200

    Fix several compilation errors

--- src/exchange_ws.rs
+++ src/exchange_ws.rs
@@ -1,10 +1,10 @@
+use crate::model::websocket::{OpenLimitsWebsocketMessage, Subscription};
 use async_trait::async_trait;
 use derive_more::Constructor;
 use futures::stream::Stream;
-use crate::model::websocket::{Subscription, OpenLimitsWebsocketMessage};
 
 use crate::shared::Result;
-use std::task::Poll;
+use std::{pin::Pin, task::Context, task::Poll};
 
 #[derive(Constructor)]
 pub struct OpenLimitsWs<E: ExchangeWs> {
@@ -12,26 +12,25 @@ pub struct OpenLimitsWs<E: ExchangeWs> {
 }
 
 #[async_trait]
-pub trait ExchangeWs {
+pub trait ExchangeWs: Stream + Unpin {
     type WebSocketMessageType;
     fn subscribe(&self, subscription: Subscription) -> Result<()>;
-    fn parse_message(&self, message: Self::WebSocketMessageType) -> Result<OpenLimitsWebsocketMessage>;
+    fn parse_message(
+        &self,
+        message: Self::WebSocketMessageType,
+    ) -> Result<OpenLimitsWebsocketMessage>;
 }
 
-impl Stream for OpenLimitsWs<ExchangeWs>  {
+impl<E: ExchangeWs> Stream for OpenLimitsWs<E> {
     type Item = Result<OpenLimitsWebsocketMessage>;
 
-    fn poll_next(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> Poll<Option<Self::Item>> {
-
-        if let Poll::Ready(Some(message)) = self.websocket.poll_next(cx) {
-            let m = self.parse_message( message?);
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if let Poll::Ready(Some(message)) = Pin::new(&mut self.websocket).poll_next(cx) {
+            let m = self.websocket.parse_message(message);
 
             return Poll::Ready(Some(m));
         }
-        
-        std::task::Poll::Pending
+
+        Poll::Pending
     }
 }
--- src/model/websocket.rs
+++ src/model/websocket.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Subscription {
