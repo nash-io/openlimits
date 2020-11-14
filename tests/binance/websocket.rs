adefc46ebbaae13cb03373717463d8224d8ec328
aurel <aurelien.catinon@gmail.com>
2020-11-14 14:53:27 +0100

    Repair generics Binance WS tests

--- tests/binance/websocket.rs
+++ tests/binance/websocket.rs
@@ -1,20 +1,38 @@
+use std::sync::mpsc::sync_channel;
+
 use openlimits::{
     binance::client::websocket::BinanceWebsocket, exchange_ws::OpenLimitsWs,
     model::websocket::Subscription,
 };
 
-#[tokio::test]
+#[tokio::test(core_threads = 2)]
 async fn orderbook() {
+    let (tx, rx) = sync_channel(0);
     let ws = init();
     let sub = Subscription::OrderBook("bnbbtc".to_string());
-    ws.subscribe(sub, |m| println!("{:?}", m)).await.unwrap();
+    ws.subscribe(sub, move |m| {
+        println!("{:?}", m);
+        tx.send(()).unwrap();
+    })
+    .await
+    .unwrap();
+
+    rx.recv().unwrap();
 }
 
-#[tokio::test]
+#[tokio::test(core_threads = 2)]
 async fn trades() {
+    let (tx, rx) = sync_channel(0);
     let ws = init();
     let sub = Subscription::Trades("btcusdt".to_string());
-    ws.subscribe(sub, |m| println!("{:?}", m)).await.unwrap();
+    ws.subscribe(sub, move |m| {
+        println!("{:?}", m);
+        tx.send(()).unwrap();
+    })
+    .await
+    .unwrap();
+
+    rx.recv().unwrap();
 }
 
 fn init() -> OpenLimitsWs<BinanceWebsocket> {
