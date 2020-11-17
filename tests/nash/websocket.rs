54049b7a0cdcb48537f299f1cac523481e75e90c
aurel <aurelien.catinon@gmail.com>
2020-11-17 19:51:24 +0100

    Fix Nash WS tests

--- tests/nash/websocket.rs
+++ tests/nash/websocket.rs
@@ -1,25 +1,37 @@
 use dotenv::dotenv;
 use openlimits::{exchange_ws::OpenLimitsWs, model::websocket::Subscription, nash::NashStream};
-use std::env;
+use std::{env, sync::mpsc::sync_channel};
 
-#[tokio::test]
+#[tokio::test(core_threads = 2)]
 async fn orderbook() {
+    let (tx, rx) = sync_channel(0);
     let client = init().await;
     let sub = Subscription::OrderBookUpdates("btc_usdc".to_string());
     client
-        .subscribe(sub, |m| println!("{:?}", m))
+        .subscribe(sub, move |m| {
+            println!("{:?}", m);
+            tx.send(()).unwrap();
+        })
         .await
         .unwrap();
+
+    rx.recv().unwrap();
 }
 
-#[tokio::test]
+#[tokio::test(core_threads = 2)]
 async fn trades() {
+    let (tx, rx) = sync_channel(0);
     let client = init().await;
     let sub = Subscription::Trades("btc_usdc".to_string());
     client
-        .subscribe(sub, |m| println!("{:?}", m))
+        .subscribe(sub, move |m| {
+            println!("{:?}", m);
+            tx.send(()).unwrap();
+        })
         .await
         .unwrap();
+
+    rx.recv().unwrap();
 }
 
 async fn init() -> OpenLimitsWs<NashStream> {
