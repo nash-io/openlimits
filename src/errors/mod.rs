ccea8bf5d5ee08d7fb62f0fa813675a68ec052d6
hermano <srhermanodecastro@gmail.com>
2021-04-29 19:57:32 -0300

    refactored module errors

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -4,73 +4,79 @@ use thiserror::Error;
 use crate::exchange::binance::BinanceContentError;
 use crate::exchange::coinbase::CoinbaseContentError;
 
-#[derive(Serialize, Deserialize, Debug, Error)]
-pub struct MissingImplementationContent {
-    pub message: String,
-}
+mod missing_implementation_content;
+mod open_limit_error;
 
-impl fmt::Display for MissingImplementationContent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "error message: {}", self.message)
-    }
-}
+pub use missing_implementation_content::MissingImplementationContent;
+pub use open_limit_error::OpenLimitsError;
 
-#[derive(Error, Debug)]
-pub enum OpenLimitsError {
-    #[error("")]
-    NoMarketPair,
-    #[error(transparent)]
-    BinanceError(#[from] BinanceContentError),
-    #[error(transparent)]
-    CoinbaseError(#[from] CoinbaseContentError),
-    #[error(transparent)]
-    NashProtocolError(#[from] nash_protocol::errors::ProtocolError),
-    #[error(transparent)]
-    MissingImplementation(#[from] MissingImplementationContent),
-    #[error("")]
-    AssetNotFound(),
-    #[error("")]
-    NoApiKeySet(),
-    #[error("")]
-    InternalServerError(),
-    #[error("")]
-    ServiceUnavailable(),
-    #[error("")]
-    Unauthorized(),
-    #[error("")]
-    SymbolNotFound(),
-    #[error("")]
-    SocketError(),
-    #[error("")]
-    WebSocketMessageNotSupported(),
-    #[error("")]
-    GetTimestampFailed(),
-    #[error(transparent)]
-    ReqError(#[from] reqwest::Error),
-    #[error(transparent)]
-    InvalidHeaderError(#[from] reqwest::header::InvalidHeaderValue),
-    #[error(transparent)]
-    InvalidPayloadSignature(#[from] serde_urlencoded::ser::Error),
-    #[error(transparent)]
-    IoError(#[from] std::io::Error),
-    #[error("")]
-    PoisonError(),
-    #[error(transparent)]
-    JsonError(#[from] serde_json::Error),
-    #[error(transparent)]
-    ParseFloatError(#[from] std::num::ParseFloatError),
-    #[error(transparent)]
-    UrlParserError(#[from] url::ParseError),
-    #[error(transparent)]
-    Tungstenite(#[from] tokio_tungstenite::tungstenite::Error),
-    #[error(transparent)]
-    TimestampError(#[from] std::time::SystemTimeError),
-    #[error("")]
-    UnkownResponse(String),
-    #[error("")]
-    NotParsableResponse(String),
-    #[error("")]
-    MissingParameter(String),
-    #[error("")]
-    InvalidParameter(String),
-}
+// #[derive(Serialize, Deserialize, Debug, Error)]
+// pub struct MissingImplementationContent {
+//     pub message: String,
+// }
+
+// impl fmt::Display for MissingImplementationContent {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "error message: {}", self.message)
+//     }
+// }
+
+// #[derive(Error, Debug)]
+// pub enum OpenLimitsError {
+//     #[error("")]
+//     NoMarketPair,
+//     #[error(transparent)]
+//     BinanceError(#[from] BinanceContentError),
+//     #[error(transparent)]
+//     CoinbaseError(#[from] CoinbaseContentError),
+//     #[error(transparent)]
+//     NashProtocolError(#[from] nash_protocol::errors::ProtocolError),
+//     #[error(transparent)]
+//     MissingImplementation(#[from] MissingImplementationContent),
+//     #[error("")]
+//     AssetNotFound(),
+//     #[error("")]
+//     NoApiKeySet(),
+//     #[error("")]
+//     InternalServerError(),
+//     #[error("")]
+//     ServiceUnavailable(),
+//     #[error("")]
+//     Unauthorized(),
+//     #[error("")]
+//     SymbolNotFound(),
+//     #[error("")]
+//     SocketError(),
+//     #[error("")]
+//     WebSocketMessageNotSupported(),
+//     #[error("")]
+//     GetTimestampFailed(),
+//     #[error(transparent)]
+//     ReqError(#[from] reqwest::Error),
+//     #[error(transparent)]
+//     InvalidHeaderError(#[from] reqwest::header::InvalidHeaderValue),
+//     #[error(transparent)]
+//     InvalidPayloadSignature(#[from] serde_urlencoded::ser::Error),
+//     #[error(transparent)]
+//     IoError(#[from] std::io::Error),
+//     #[error("")]
+//     PoisonError(),
+//     #[error(transparent)]
+//     JsonError(#[from] serde_json::Error),
+//     #[error(transparent)]
+//     ParseFloatError(#[from] std::num::ParseFloatError),
+//     #[error(transparent)]
+//     UrlParserError(#[from] url::ParseError),
+//     #[error(transparent)]
+//     Tungstenite(#[from] tokio_tungstenite::tungstenite::Error),
+//     #[error(transparent)]
+//     TimestampError(#[from] std::time::SystemTimeError),
+//     #[error("")]
+//     UnkownResponse(String),
+//     #[error("")]
+//     NotParsableResponse(String),
+//     #[error("")]
+//     MissingParameter(String),
+//     #[error("")]
+//     InvalidParameter(String),
+// }
---
+++ src/errors/open_limit_error.rs
@@ -0,0 +1,64 @@
+use thiserror::Error;
+use crate::exchange::binance::BinanceContentError;
+use crate::exchange::coinbase::CoinbaseContentError;
+use super::MissingImplementationContent;
+
+#[derive(Error, Debug)]
+pub enum OpenLimitsError {
+    #[error("")]
+    NoMarketPair,
+    #[error(transparent)]
+    BinanceError(#[from] BinanceContentError),
+    #[error(transparent)]
+    CoinbaseError(#[from] CoinbaseContentError),
+    #[error(transparent)]
+    NashProtocolError(#[from] nash_protocol::errors::ProtocolError),
+    #[error(transparent)]
+    MissingImplementation(#[from] MissingImplementationContent),
+    #[error("")]
+    AssetNotFound(),
+    #[error("")]
+    NoApiKeySet(),
+    #[error("")]
+    InternalServerError(),
+    #[error("")]
+    ServiceUnavailable(),
+    #[error("")]
+    Unauthorized(),
+    #[error("")]
+    SymbolNotFound(),
+    #[error("")]
+    SocketError(),
+    #[error("")]
+    WebSocketMessageNotSupported(),
+    #[error("")]
+    GetTimestampFailed(),
+    #[error(transparent)]
+    ReqError(#[from] reqwest::Error),
+    #[error(transparent)]
+    InvalidHeaderError(#[from] reqwest::header::InvalidHeaderValue),
+    #[error(transparent)]
+    InvalidPayloadSignature(#[from] serde_urlencoded::ser::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error("")]
+    PoisonError(),
+    #[error(transparent)]
+    JsonError(#[from] serde_json::Error),
+    #[error(transparent)]
+    ParseFloatError(#[from] std::num::ParseFloatError),
+    #[error(transparent)]
+    UrlParserError(#[from] url::ParseError),
+    #[error(transparent)]
+    Tungstenite(#[from] tokio_tungstenite::tungstenite::Error),
+    #[error(transparent)]
+    TimestampError(#[from] std::time::SystemTimeError),
+    #[error("")]
+    UnkownResponse(String),
+    #[error("")]
+    NotParsableResponse(String),
+    #[error("")]
+    MissingParameter(String),
+    #[error("")]
+    InvalidParameter(String),
+}
