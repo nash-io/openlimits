94c989025d423d6e5c51d3af693e369248b890a5
Danilo Guanabara <danilo@sensorial.systems>
2021-08-09 12:43:30 -0300

    Adding order filters

--- src/exchange/traits/exchange_account.rs
+++ src/exchange/traits/exchange_account.rs
@@ -8,6 +8,7 @@ use crate::{
     },
 };
 use super::shared::Result;
+use crate::model::OrderFilter;
 
 #[async_trait]
 pub trait ExchangeAccount {
@@ -22,4 +23,24 @@ pub trait ExchangeAccount {
     async fn get_trade_history(&self, req: &TradeHistoryRequest) -> Result<Vec<Trade>>;
     async fn get_account_balances(&self, paginator: Option<Paginator>) -> Result<Vec<Balance>>;
     async fn get_order(&self, req: &GetOrderRequest) -> Result<Order>;
+
+    async fn get_open_orders(&self, order_filter: &OrderFilter) -> Result<Vec<Order>> {
+        Ok(self
+            .get_all_open_orders()
+            .await?
+            .into_iter()
+            .filter(|order| {
+                order_filter.client_order_id.as_ref().map(|client_order_id| Some(client_order_id) == order.client_order_id.as_ref()).unwrap_or(true) &&
+                order_filter.order_type.as_ref().map(|order_type| *order_type == order.order_type).unwrap_or(true) &&
+                order_filter.market_pair.as_ref().map(|market_pair| *market_pair == order.market_pair).unwrap_or(true) &&
+                order_filter.side.as_ref().map(|side| *side == order.side).unwrap_or(true) &&
+                order_filter.status.as_ref().map(|status| *status == order.status).unwrap_or(true) &&
+                order_filter.created_at.as_ref().map(|created_at| order.created_at >= Some(created_at.start) && order.created_at <= Some(created_at.end)).unwrap_or(true) &&
+                order_filter.price.as_ref().map(|price| order.price.as_ref() >= Some(&price.start) && order.price.as_ref() <= Some(&price.end)).unwrap_or(true) &&
+                order_filter.remaining.as_ref().map(|remaining| order.remaining.as_ref() >= Some(&remaining.start) && order.remaining.as_ref() <= Some(&remaining.end)).unwrap_or(true) &&
+                order_filter.size.as_ref().map(|size| order.size >= size.start && order.size <= size.end).unwrap_or(true)
+            })
+            .collect()
+        )
+    }
 }
