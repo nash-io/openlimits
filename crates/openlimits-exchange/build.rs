d9ae21e56ea85c948d1bdb091c3dcc354e37bc53
Danilo Guanabara <danilo@sensorial.systems>
2021-08-24 01:40:56 -0300

    Building first files

--- crates/openlimits-exchange/build.rs
+++ crates/openlimits-exchange/build.rs
@@ -1,14 +1,17 @@
 use ligen::prelude::*;
-use ligen_c::CGenerator;
-use ligen_cmake::{CMakeGenerator, Language};
+// use ligen_c::CGenerator;
+// use ligen_cmake::{CMakeGenerator, Language};
+use ligen_csharp::CSharpGenerator;
 use ligen::ir::Module;
 
 fn main() {
     let module = Module::from_path("src/model/candle.rs").expect("Couldn't read module.");
     if let Ok(project) = Project::read_from_module(module) {
-        let c_generator = CGenerator::default();
-        let cmake_generator = CMakeGenerator::new(Language::C);
-        cmake_generator.generate(&project).expect("Couldn't generate CMake project.");
-        c_generator.generate(&project).expect("Couldn't generate C bindings");
+        // let c_generator = CGenerator::default();
+        // let cmake_generator = CMakeGenerator(Language::C);
+        let csharp_generator = CSharpGenerator::default();
+        // cmake_generator.generate(&project).expect("Couldn't generate CMake project.");
+        // c_generator.generate(&project).expect("Couldn't generate C bindings.");
+        csharp_generator.generate(&project).expect("Couldn't generate C# bindings.")
     }
 }
